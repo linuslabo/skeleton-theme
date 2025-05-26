86738f4db629bd95996afaab261be056caa85d5b
Salvo <dev@lanscodesign.com>
2025-05-26 15:26:46 +0200

    + alpine + swiper deps

--- frontend/entrypoints/main.js
+++ frontend/entrypoints/main.js
@@ -1,1 +1,20 @@
 import 'vite/modulepreload-polyfill'
+
+import Alpine from 'alpinejs';
+import { collapse } from '@alpinejs/collapse';
+import persist from '@alpinejs/persist';
+import { intersect } from '@alpinejs/intersect';
+import Swiper from 'swiper';
+import { Autoplay, Navigation, EffectFade } from 'swiper/modules';
+import 'swiper/css';
+
+Alpine.plugin(collapse);
+Alpine.plugin(persist);
+Alpine.plugin(intersect);
+
+window.Alpine = Alpine;
+window.Swiper = Swiper;
+window.SwiperNavigation = Navigation;
+window.SwiperAutoplay = Autoplay;
+window.SwiperEffectFade = EffectFade;
+Alpine.start();
